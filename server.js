c3eea242083b90a0038dabf10c49cec0ee5e8a2c
Camila Doriguello <54558319+camiladoriguello@users.noreply.github.com>
2021-06-22 21:47:23 -0300

    correcting server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/marvelApp'))
+app.use(express.static(__dirname + '/dist/marvelApp'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(__dirname + '/dist/marvelApp/index.html')
-})
+    res.sendFile(__dirname + '/dist/marvelApp/index.html');
+});
 
 app.listen(PORT, () => {
     console.log('Servidor iniciado na porta ' + PORT);
-})
+});
